97f1a6c77c2d829d13129aa29912a291419693e6
MadMehx <65829543+MadMehx@users.noreply.github.com>
2022-08-09 18:48:00 -0700

    init

---
+++ PBv1/PBv1/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace PBv1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblRec = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(0, 0);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnRecord.TabIndex = 0;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(81, 0);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(162, 0);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // lblRec
+            // 
+            this.lblRec.AutoSize = true;
+            this.lblRec.Location = new System.Drawing.Point(12, 42);
+            this.lblRec.Name = "lblRec";
+            this.lblRec.Size = new System.Drawing.Size(81, 13);
+            this.lblRec.TabIndex = 3;
+            this.lblRec.Text = "Voice Recorder";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(188, 42);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(49, 13);
+            this.lblTimer.TabIndex = 4;
+            this.lblTimer.Text = "00:00:00";
+            this.lblTimer.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 73);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.lblRec);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRecord);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lbl_timer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_rec;
+        private System.Windows.Forms.Button btn_record;
+        private System.Windows.Forms.Button btn__play;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblRec;
+        private System.Windows.Forms.Label lblTimer;
+    }
+}
+
---
+++ PBv1/PBv1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+
+namespace PBv1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("winmm.dll")]
+        private static extern int mciSendString(string MciComando, string MciRetorno, int MciRetornoLeng, int CallBack);
+        string record = "";
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+
+            mciSendString("open new type waveaudio alias Som", null, 0, 0);
+            mciSendString("record Som", null, 0, 0);
+            lbl_rec.Text = "Recording....";
+            lbl_rec.ForeColor = System.Drawing.Color.Red;
+            timer1.Start();
+            Count_Down.Start();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            mciSendString("pause Som", null, 0, 0);
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "wave|*.wav";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                mciSendString("save Som " + sfd.FileName, null, 0, 0);
+                mciSendString("close Som", null, 0, 0);
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (record == "")
+            {
+
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "wave|*.wav";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    record = ofd.FileName;
+                }
+            }
+            mciSendString("play " + record, null, 0, 0);
+            lblRec.Text = "Playing....";
+        }
+
+        System.Diagnostics.Stopwatch Count_Down = new System.Diagnostics.Stopwatch();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = Count_Down.Elapsed;
+            lblTimer.Text = "Duration :" + string.Format("{0:00}:{1:00}:{2:00}", Math.Floor(elapsed.TotalHours),
+                elapsed.Minutes, elapsed.Seconds);
+        }
+
+    }
+}
